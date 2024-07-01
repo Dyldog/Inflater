6fe25f0e9ce86dffc6acca93fafa78b2902508ef
Dylan Elliott <dylan.elliott@gmail.com>
2024-07-01 17:23:57 +1000

    Format files

--- Inflater/ContentView.swift
+++ Inflater/ContentView.swift
@@ -3,7 +3,6 @@
 //  Inflater
 //
 //  Created by Dylan Elliott on 27/6/2024.
-//
 
 import SwiftUI
 
@@ -11,7 +10,7 @@ struct IntSlider: View {
     let value: Binding<Int>
     let range: ClosedRange<Int>
     let onEditingChanged: (Bool) -> Void
-    
+
     init(
         value: Binding<Int>,
         in range: ClosedRange<Int>,
@@ -21,6 +20,7 @@ struct IntSlider: View {
         self.range = range
         self.onEditingChanged = onEditingChanged
     }
+
     var body: some View {
         Slider(
             value: .init(get: {
@@ -28,47 +28,47 @@ struct IntSlider: View {
             }, set: { new in
                 value.wrappedValue = Int(new.rounded())
             }),
-            in: (Double(range.lowerBound) ... Double(range.upperBound)),
+            in: Double(range.lowerBound) ... Double(range.upperBound),
             onEditingChanged: onEditingChanged
         )
     }
 }
 
 struct ContentView: View {
-    
     let inflationManager: InflationDataManager
-    
+
     @State var pastAmount: Int
     @State var startYear: Int
-    
+
     var endYear: Int { inflationManager.endYear }
-    
+
     var currentInflation: Double {
         inflationManager.totalInflation(from: startYear, to: endYear)
     }
+
     var currentAmount: Double {
         Double(pastAmount) * currentInflation
     }
-    
+
     var amountFormatter: NumberFormatter {
         let format = NumberFormatter()
         format.numberStyle = .currency
         return format
     }
-    
+
     var yearFormatter: NumberFormatter {
         let format = NumberFormatter()
         format.minimum = inflationManager.startYear as NSNumber
         format.maximum = inflationManager.endYear as NSNumber
         return format
     }
-    
+
     init(dataManager: InflationDataManager) {
-        self.inflationManager = dataManager
-        self.pastAmount = 1
-        self.startYear = inflationManager.startYear
+        inflationManager = dataManager
+        pastAmount = 1
+        startYear = inflationManager.startYear
     }
-    
+
     var body: some View {
         VStack(alignment: .center) {
             HStack {
@@ -76,14 +76,14 @@ struct ContentView: View {
                     .frame(minWidth: 0)
                 Text(formatted("in"))
             }
-            
+
             HStack {
                 TextField("Start Year", value: $startYear, formatter: yearFormatter)
                 Text(formatted("money"))
             }
-            
+
             JustifiedText(formatted("is \(amountFormatter.string(from: currentAmount as NSNumber)!) in"))
-            
+
             JustifiedText(formatted("\(endYear) money"))
         }
         .font(.largeTitle)
@@ -92,7 +92,7 @@ struct ContentView: View {
         .padding()
         .padding(.horizontal, 30)
     }
-    
+
     func formatted(_ text: String) -> String {
         text.uppercased()
     }
